903b217f0ff253782bd8edadc9645d734fc214d3
hazelanncreator <solanohazel123@gmail.com>
2025-09-17 19:28:33 +0800

    website routes blueprint done

--- routes/website.py
+++ routes/website.py
@@ -2,23 +2,26 @@ from flask import Blueprint, render_template
 
 website_bp = Blueprint("website",__name__,template_folder="../templates/website")
 
-
-@app.route("/")
-def home():
-  return render_template("website/home.html")
-
-@app.route("/login")
-def login():
-  return render_template("login.html")
-
-@app.route("/courses")
-def courses():
-  return render_template("website/courses.html")
-
-@app.route("/enrollment")
-def enrollment():
-  return render_template("enrollment.html")
-
-@app.route("/base")
+@website_bp("/")
 def base():
   return render_template("website/base.html")
+
+@website_bp("/about")
+def about():
+  return render_template("website/about.html")
+
+@website_bp("/academics")
+def academics():
+  return render_template("website/academics.html")
+
+@website_bp("/admission")
+def admission():
+  return render_template("website/admission.html")
+
+@website_bp("/contact")
+def contact():
+  return render_template("website/contact.html")
+
+@website_bp("/home")
+def home():
+  return render_template("website/home.html")
